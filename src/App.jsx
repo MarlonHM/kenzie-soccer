e25bc05827fc48038c0584f8828b748d6c2c0eb0
Micaela/Bluemoono <mikka_paulina@live.com>
2022-09-09 19:39:47 -0300

    Feat: criação do componente Button com suas variações

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Button from "./components/Button";
 import Routes from "./routes";
 import GlobalStyle from "./styles";
 
---
+++ src/components/Button/style.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const ButtonColor = styled.button`
+  background: ${props =>
+    (props.primary && (props.primary ? "var(--wine)" : "white")) ||
+    (props.secondary && (props.secondary ? "var(--light-pink)" : "white")) ||
+    (props.tertiary ? "var(--wine-2)" : "white")};
+
+  color: ${props => (props.secondary ? "var(--wine-2)" : "var(--white)")};
+  font-size: 1.2em;
+  margin: 1vw;
+  padding: 1vw 1.5vw;
+  border: none;
+  border-radius: 0.2em;
+  display: flex;
+
+  @media (min-width: 360px) and (max-width: 650px) {
+    border-radius: 0.3em;
+    padding: 3vw;
+    font-size: 1em;
+  }
+`;
