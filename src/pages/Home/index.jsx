f6ee34293c9dea569d0d4a7b904a3f702a5b6383
Jaqueline Pereira Silva <jaque.perera@gmail.com>
2022-09-20 21:05:41 -0300

    feature readme

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -8,16 +8,10 @@ import RulesAndBenefits from "../../components/RulesAndBenefits";
 import DevelopersTeam from "../../components/DevelopersTeam";
 import Footer from "../../components/Footer";
 
-import Modal from "../../components/Modal";
-
 const Home = () => {
-  const [modalState, setModalState] = useState(false);
-
   return (
     <Container>
       <Header />
-
-      <button onClick={() => setModalState(true)}>Abrir Modal de testes</button>
 
       <main>
         <About />
@@ -27,17 +21,6 @@ const Home = () => {
       </main>
 
       <Footer />
-
-      {modalState && (
-        <Modal
-          title="Modal de teste"
-          modalState={modalState}
-          setModalState={setModalState}
-        >
-          {/* aqui vai o conteúdo do corpo do modal */}
-          <p>Mudar nome do jogador</p>
-        </Modal>
-      )}
     </Container>
   );
 };
